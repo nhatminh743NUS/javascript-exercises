ea181158aee8d7d2dfc10cc95429f3036b5a05bc
Nhat Minh Pham <e1375578@u.nus.edu>
2025-06-16 20:39:42 +0700

    Do these problems

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(var1, var2) {
+	let sum = var1 + var2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(var1, var2) {
+	let sum = var1 - var2;
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function(array1) {
+	let total = 0;
+  for (let number of array1) {
+    total += number;
+  }
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for (let number of array) {
+    total *= number;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(var1, var2) {
+	let total = var1 ** var2;
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let i = 1;
+  let total = 1;
+  while (i <= number) {
+    total *= i;
+    i++;
+  }
+  return total;
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleaned = string.toLowerCase().replace(/[^a-z0-9]/g, '')
+    let final = cleaned.split('').reverse().join('');
+    return final === cleaned;
 };
 
 // Do not edit below this line
